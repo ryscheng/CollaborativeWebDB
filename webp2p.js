f25b522a8ed5e8ea94595569a522de4f0baefd93
Raymond Cheng <RaymondYSCheng@gmail.com>
2012-03-27 16:58:09 -0700

    Start

---
+++ webp2p.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  //Show alert on click
+  $('video').click(function() {
+    alert("Hello world!");
+  });
+
+  //Replace the source of the video element with the stream from the camera
+  navigator.getUserMedia_ = navigator.getUserMedia || navigator.webkitGetUserMedia;
+  if(!!navigator.getUserMedia_ !== false) {
+    navigator.getUserMedia_("video", successCallback, errorCallback);
+    function successCallback(stream) {
+      console.log(stream)
+      var video = $('video')[0]
+      //video.src = window.webkitURL ? window.webkitURL.createObjectURL(stream) : stream;
+    }
+    function errorCallback(error) {
+      console.error('An error occurred: [CODE ' + error.code + ']');
+      return;
+    }
+  } else {
+    console.log('Native web camera streaming (getUserMedia) is not supported in this browser.');
+    $('output').html('Sorry, your browser doesn\'t support getUserMedia. ')
+      .append('<p>Try Chrome canary or dev channel ')
+      .append('with enabling MediaStream at chrome://flags ')
+      .append('(To be sure that it is now experimental ')
+      .append("and don't forget to set --enable-media-stream ")
+      .append("as a execute parameter)")
+      .append('</p>')
+    return;
+  }
+
+});
