0cfd714345b14a77225efac98939265ef619b7c3
Will Scott <willscott@gmail.com>
2012-05-21 14:38:58 -0700

    simpler compose pane flow.

--- js/compose_pane.js
+++ js/compose_pane.js
@@ -4,11 +4,12 @@ var compose_pane = {
     // initialization ends up with a different code path, and the status callbacks
     // are more interesting than the data / completion ones.
     database.status_cb = compose_pane.update_status;
-    database.exec("SELECT * FROM SQLITE_MASTER;", true, function() {}, function() {});
+    database.exec("SELECT * FROM SQLITE_MASTER;", true, compose_pane.finish_init, function() {});
     $('#compose .bar').css('width', "10%");
   },
   finish_init: function() {
     log.write("Database loaded, reading tables.");
+    database.status_cb = null;
     database.backing(compose_pane.render_sqlite);
   },
   render_sqlite: function(s) {
@@ -37,8 +38,6 @@ var compose_pane = {
       var ci = ++compose_pane._seen;
       var percent = 100 * ci / msg['n'];
       $('#compose .bar').css('width', percent + "%");
-    } else if (msg == "Database Initialized") {
-      window.setTimeout(compose_pane.finish_init, 1000);
     }
   },
   render_table: function(table) {
