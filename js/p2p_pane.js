8e1bb00a633245f629a8282057a4452cd1ed78cc
Will Scott <willscott@gmail.com>
2012-05-24 13:49:14 -0700

    Update the p2p_pane to setup extension communication

--- js/p2p_pane.js
+++ js/p2p_pane.js
@@ -1,4 +1,5 @@
 var p2p_pane = {
+    _lsid: "p2p_extension_id",
     init: function() {    
       var title = document.createElement("a");
       $(title).attr('href','#p2p').attr('data-toggle','tab').html("P2P");
@@ -11,20 +12,38 @@ var p2p_pane = {
       $("#results>.tab-content").append(container);
 
       this.el = $('#p2p');
-      
-      var box = document.createElement('input');
-      this.el.append(box);
-      
-      var connectButton = document.createElement('button');
-      $(connectButton).text("Connect").click(function(e) {
-        e.preventDefault();
-        var addr = "chrome-extension://" + box.value + "/load.html";
+     
+      var id = localStorage.getItem(p2p_pane._lsid); 
+      if (id) {
+        p2p_pane.setup(id);
+      } else {
+        var snippet = "<form class='well'><input type='text' class='span3' placeholder='lhoiocpbddbgjmkkamijhlmhbieacodc'><button type='submit' class='btn'>Connect</button></form>";
+        this.el[0].innerHTML = "<div class='alert'><strong>Warning</strong> Extension not connected!" +
+            "<br />To connect the extension, go to chrome://extensions, and copy the ID here.</div>" + snippet;
+        
+        var box = this.el[0].getElementsByTagName('input')[0];
+        var button = this.el[0].getElementsByTagName('button')[0];
+        
+        $(button).click(function(e) {
+          e.preventDefault();
+          localStorage.setItem(p2p_pane._lsid, box.value);
+          p2p_pane.setup(box.value);
+          return false;
+        });
+      }
+    },
+    setup: function(id) {
+        var addr = "chrome-extension://" + id + "/load.html";
         var req_fr = document.createElement('iframe');
         req_fr.src = addr;
-        $('#p2p').append(req_fr);
         req_fr.style.display='none';
-        return false;
-      });
-      this.el.append(connectButton);
+        document.body.appendChild(req_fr);
+        $('#p2p')[0].innerHTML = "<p>Extension Communication Log</p><pre id='p2plog'></pre>";
+        window.addEventListener('message', p2p_pane.msgTracker, false);
+    },
+    msgTracker: function(evt) {
+      if (evt.data && evt.data.to) {
+        $('#p2plog')[0].innerHTML += '\n' + JSON.stringify(evt.data).substr(0,80);
+      }
     }
 };
