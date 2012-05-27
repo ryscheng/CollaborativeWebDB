a6ab98b5f5fc80cb4b53d5ac2c783ee5decda69f
Will Scott <willscott@gmail.com>
2012-05-26 18:10:41 -0700

    random table now chosen

--- server/querygen/engine.py
+++ server/querygen/engine.py
@@ -1,14 +1,21 @@
 import sqlite3
+import random
 
 class Engine:
   """ Creates a querygen Engine against a provided DB """
   def __init__(self, db):
     self.db = db;
+    self.tables = []
+    self.schemas = {}
+    self.learnSchema()
 
   def getQuery(self):
-    return "SELECT * FROM table LIMIT 0,30";
+    table = self.getTable()
+    return "SELECT * FROM " + table + " LIMIT 0,30";
 
   def learnSchema(self):
-    cursor = self.db.cursor()
-    cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
-    cursor.fetchall()
+    for row in self.db.execute("SELECT * FROM sqlite_master WHERE type='table';"):
+      self.tables.append(row[1])
+
+  def getTable(self):
+    return random.choice(self.tables)
