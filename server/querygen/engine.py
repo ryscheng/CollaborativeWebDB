6b87379fd06fca2dd0c24a52bca34261346875ca
Will Scott <willscott@gmail.com>
2012-05-26 18:31:53 -0700

    now has column selection

--- server/querygen/engine.py
+++ server/querygen/engine.py
@@ -11,11 +11,18 @@ class Engine:
 
   def getQuery(self):
     table = self.getTable()
-    return "SELECT * FROM " + table + " LIMIT 0,30";
+    cols = self.getColumns(table)
+    return "SELECT " + ','.join(cols) + " FROM " + table + " LIMIT 0,30";
+
+  def getTable(self):
+    return random.choice(self.tables)
+
+  def getColumns(self, table):
+    num = random.randint(1, len(self.schemas[table]))
+    return [col[1] for col in random.sample(self.schemas[table], num)]
 
   def learnSchema(self):
     for row in self.db.execute("SELECT * FROM sqlite_master WHERE type='table';"):
       self.tables.append(row[1])
-
-  def getTable(self):
-    return random.choice(self.tables)
+      self.schemas[row[1]] = [col for col in self.db.execute("PRAGMA table_info("+row[1]+");")]
+    
